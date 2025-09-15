2abb8b604a57bfa26479ba6f38dacc563d58753e
Ben Picha <ben.picha@web.de>
2025-09-15 18:12:58 +0200

    Initial Commit

---
+++ Schul-Coach AI/Schul_Coach_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Schul_Coach_AIApp.swift
+//  Schul-Coach AI
+//
+//  Created by Ben Picha on 15.09.25.
+//
+
+import SwiftUI
+
+@main
+struct Schul_Coach_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
